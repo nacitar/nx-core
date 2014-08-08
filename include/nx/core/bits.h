7277d8dd1506a32fd298d0e2c676b8b4f1796ab2
Jacob McIntosh <nacitar@ubercpp.com>
2014-08-08 15:45:22 -0500

    Updating progress on bits.

--- include/nx/core/bits.h
+++ include/nx/core/bits.h
@@ -17,6 +17,7 @@
 /// @file bits.h
 /// @brief Provides wrappers for bitwise operations.
 
+// TODO: question use of inlining
 #ifndef INCLUDE_NX_BITS_H_
 #define INCLUDE_NX_BITS_H_
 
@@ -26,17 +27,22 @@
 /// @brief Library namespace.
 namespace nx {
 
+/// @brief Bits namespace.
+namespace bits {
+
 /// @cond nx_detail
 namespace detail {
 
-#if defined(NX_TC_GCC)
-
 ////
+// Dynamic implementations
+
+#if defined(NX_TC_GCC) || defined (NX_TC_CLANG)
+
 // Forward
 
 /// @brief unsigned long long version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long long>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned long>>>,  // NOLINT(runtime/int)
@@ -48,7 +54,7 @@ unsigned int> BitScanForward(T value) {
 
 /// @brief unsigned long version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned int>>>,
@@ -60,7 +66,7 @@ unsigned int> BitScanForward(T value) {
 
 /// @brief unsigned int version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned int>>,
 unsigned int> BitScanForward(T value) {
@@ -69,12 +75,11 @@ unsigned int> BitScanForward(T value) {
       : 0);
 }
 
-////
 // Reverse
 
 /// @brief unsigned long long version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long long>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned long>>>,  // NOLINT(runtime/int)
@@ -86,7 +91,7 @@ unsigned int> BitScanReverse(T value) {
 
 /// @brief unsigned long version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned int>>>,
@@ -98,7 +103,7 @@ unsigned int> BitScanReverse(T value) {
 
 /// @brief unsigned int version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned int>>,
 unsigned int> BitScanReverse(T value) {
@@ -107,12 +112,11 @@ unsigned int> BitScanReverse(T value) {
       : 0);
 }
 
-////
 // PopCount
 
 /// @brief unsigned long long version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long long>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned long>>>,  // NOLINT(runtime/int)
@@ -123,7 +127,7 @@ unsigned int> PopCount(T value) {
 
 /// @brief unsigned long version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned int>>>,
@@ -134,7 +138,7 @@ unsigned int> PopCount(T value) {
 
 /// @brief unsigned int version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned int>>,
 unsigned int> PopCount(T value) {
@@ -143,12 +147,12 @@ unsigned int> PopCount(T value) {
 }
 
 #elif defined(NX_TC_VS)
-////
+
 // Forward
 
 /// @brief unsigned __int64 version
 template <class T>
-static NX_FORCEINLINE EnableIf<All<
+NX_FORCEINLINE EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned __int64>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned long>>>,
@@ -161,7 +165,7 @@ unsigned int> BitScanForward(T value) {
 
 /// @brief unsigned long version
 template <class T>
-static NX_FORCEINLINE EnableIf<All<
+NX_FORCEINLINE EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long>>,  // NOLINT(runtime/int)
 unsigned int> BitScanForward(T value) {
@@ -171,12 +175,11 @@ unsigned int> BitScanForward(T value) {
   return static_cast<unsigned int>(index);
 }
 
-////
 // Reverse
 
 /// @brief unsigned __int64 version
 template <class T>
-static NX_FORCEINLINE EnableIf<All<
+NX_FORCEINLINE EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned __int64>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned long>>>,
@@ -189,7 +192,7 @@ unsigned int> BitScanReverse(T value) {
 
 /// @brief unsigned long version
 template <class T>
-static NX_FORCEINLINE EnableIf<All<
+NX_FORCEINLINE EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned long>>,  // NOLINT(runtime/int)
 unsigned int> BitScanReverse(T value) {
@@ -199,12 +202,11 @@ unsigned int> BitScanReverse(T value) {
   return static_cast<unsigned int>(index);
 }
 
-////
 // PopCount
 
 /// @brief unsigned __int64 version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned __int64>,  // NOLINT(runtime/int)
       Not<IntegerFits<T, unsigned int>>>,
@@ -215,7 +217,7 @@ unsigned int> PopCount(T value) {
 
 /// @brief unsigned int version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned int>,
       Not<IntegerFits<T, unsigned short>>>,  // NOLINT(runtime/int)
@@ -226,105 +228,365 @@ unsigned int> PopCount(T value) {
 
 /// @brief unsigned short version
 template <class T>
-static NX_FORCEINLINE constexpr EnableIf<All<
+NX_FORCEINLINE constexpr EnableIf<All<
       std::is_integral<T>,
       IntegerFits<T, unsigned short>>,  // NOLINT(runtime/int)
 unsigned int> PopCount(T value) {
   return __popcnt16(
       static_cast<unsigned short>(value));  // NOLINT(runtime/int)
 }
+
 #else
+
 #error "Your platform requires an implementation of BSR/BSF."
+// to reduce errors, define dummies
+template <class T> unsigned int BitScanForward(T value) { return 0; }
+template <class T> unsigned int BitScanReverse(T value) { return 0; }
+template <class T> unsigned int PopCount(T value) { return 0; }
+
 #endif
 
+////
+// Static implementations
+
 template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<value_ && !(value_ & 1)>,
-    unsigned int> BitScanForward() {
-  return UInt<1 + BitScanForward<
-      (static_cast<typename std::make_unsigned<T>::type>(value_) >> 1)>
-      >::value;
-}
-template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<!value_ || value_ & 1>,
+NX_FORCEINLINE constexpr EnableIf<Bool<!value_ || value_ & static_cast<T>(1)>,
     unsigned int> BitScanForward() {
   return 0;
 }
-
-
 template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<
-    Bool<value_ && value_ != static_cast<T>(0x1)>,
-    unsigned int> BitScanReverse() {
-  return UInt<1 + BitScanReverse<
-      (static_cast<typename std::make_unsigned<T>::type>(value_) >> 1)>
-      >::value;
+NX_FORCEINLINE constexpr EnableIf<Bool<value_ && !(value_ & static_cast<T>(1))>,
+    unsigned int> BitScanForward() {
+  typedef typename std::make_unsigned<T>::type unsigned_T;
+  return 1 +
+      BitScanForward<unsigned_T, (static_cast<unsigned_T>(value_) >> 1)>();
 }
-template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<value_ == static_cast<T>(1)>,
-    unsigned int> BitScanReverse() {
-  return 0;
-}
+
 // Give a defined result
 template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<!value_>,
+NX_FORCEINLINE constexpr EnableIf<Bool<!value_>,
     unsigned int> BitScanReverse() {
   return 0;
 }
-
 template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<value_ && (value_ & 1)>,
-    unsigned int> PopCount() {
-  return UInt<1 + PopCount<
-      (static_cast<typename std::make_unsigned<T>::type>(value_) >> 1)>
-      >::value;
+NX_FORCEINLINE constexpr EnableIf<Bool<value_ == static_cast<T>(1)>,
+    unsigned int> BitScanReverse() {
+  return 0;
+}
+template <class T, T value_>
+NX_FORCEINLINE constexpr EnableIf<
+    Bool<value_ && value_ != static_cast<T>(1)>,
+    unsigned int> BitScanReverse() {
+  typedef typename std::make_unsigned<T>::type unsigned_T;
+  return 1 +
+      BitScanReverse<unsigned_T, (static_cast<unsigned_T>(value_) >> 1)>();
 }
 
 template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<value_ && !(value_ & 1)>,
-    unsigned int> PopCount() {
-  return UInt<PopCount<
-      (static_cast<typename std::make_unsigned<T>::type>(value_) >> 1)>
-      >::value;
-}
-template <class T, T value_>
-static NX_FORCEINLINE constexpr EnableIf<Bool<!value_>,
+NX_FORCEINLINE constexpr EnableIf<Bool<value_ == static_cast<T>(0)>,
     unsigned int> PopCount() {
   return 0;
 }
+template <class T, T value_>
+NX_FORCEINLINE constexpr EnableIf<Bool<value_ != static_cast<T>(0)>,
+    unsigned int> PopCount() {
+  typedef typename std::make_unsigned<T>::type unsigned_T;
+  return (value_ & 1) +
+      PopCount<unsigned_T, (static_cast<unsigned_T>(value_) >> 1)>();
+}
+
+
+template <class T>
+NX_FORCEINLINE constexpr T Mask() {
+  return 0;
+}
+template <class T, typename ... Arguments>
+NX_FORCEINLINE constexpr T Mask(unsigned int index_, Arguments ... rest_) {
+  return (static_cast<T>(1u) << index_) | Mask<T>(rest_...);
+}
+template <class T,unsigned int index_, unsigned int ... rest_>
+NX_FORCEINLINE constexpr T Mask() {
+  return std::integral_constant<T,Mask<T>(index_, rest_...)>::value;
+}
+
 
 }  // namespace detail
 /// @endcond
 
+// returns 0 for 0
+template <typename T>
+NX_FORCEINLINE unsigned int BitScanForward(T value) {
+  return detail::BitScanForward(value);
+}
+// returns 0 for 0
+template <typename T>
+NX_FORCEINLINE unsigned int BitScanReverse(T value) {
+  return detail::BitScanReverse(value);
+}
+template <typename T>
+NX_FORCEINLINE unsigned int PopCount(T value) {
+  return detail::PopCount(value);
+}
+template <typename T, T value_>
+NX_FORCEINLINE constexpr unsigned int BitScanForward() {
+  return detail::BitScanForward<T, value_>();
+}
+template <typename T, T value_>
+NX_FORCEINLINE constexpr unsigned int BitScanReverse() {
+  return detail::BitScanReverse<T, value_>();
+}
+template <typename T, T value_>
+NX_FORCEINLINE constexpr unsigned int PopCount() {
+  return detail::PopCount<T, value_>();
+}
+
+template <typename T>
+NX_FORCEINLINE constexpr unsigned int Size() {
+  return UInt<static_cast<unsigned int>(sizeof(T)*CHAR_BIT)>::value;
+}
+
+template <typename T>
+NX_FORCEINLINE constexpr bool MultiplicationOverflow(T kLHS, T kRHS) {
+  return (kRHS != 0 && (static_cast<T>(kLHS * kRHS) / kRHS) != kLHS);
+}
+/// @brief Determines if multiplying kLHS with kRHS will result in an overflow.
+template <typename T, T kLHS_, T kRHS_>
+NX_FORCEINLINE constexpr bool MultiplicationOverflow() {
+  return Bool<MultiplicationOverflow(kLHS_,kRHS_)>::value;
+}
+
+template <typename T>
+NX_FORCEINLINE constexpr bool PowerOfTwo(T value) {
+  return (value && !(value & (value - 1)));
+}
+template <typename T, T value_>
+NX_FORCEINLINE constexpr bool PowerOfTwo() {
+  return Bool<PowerOfTwo(value_)>::value;
+}
+
+template <class T, unsigned int ... indexes_>
+NX_FORCEINLINE constexpr T Mask() {
+  return detail::Mask<T,indexes_...>();
+}
+
+template <class T, typename ... Arguments>
+NX_FORCEINLINE constexpr T Mask(Arguments... indexes_) {
+  return detail::Mask<T>(indexes_...);
+}
+
+}  // namespace bits
+
+// TODO: make only work for integral values
+
+// BitRange/IntegerFits/Power
+// LowBitMask
 template <typename T>
 class BitUnMasked {
  public:
+  typedef T type;
   // returns 0 for 0
   static NX_FORCEINLINE unsigned int BitScanForward(T value) {
-    return detail::BitScanForward(value);
+    return bits::BitScanForward<T>(value);
   }
   // returns 0 for 0
   static NX_FORCEINLINE unsigned int BitScanReverse(T value) {
-    return detail::BitScanReverse(value);
+    return bits::BitScanReverse<T>(value);
   }
   static NX_FORCEINLINE unsigned int PopCount(T value) {
-    return detail::PopCount(value);
+    return bits::PopCount<T>(value);
   }
+  static NX_FORCEINLINE constexpr bool MultiplicationOverflow(T kLHS, T kRHS) {
+    return bits::MultiplicationOverflow<T>(kLHS,kRHS);
+  }
+  static NX_FORCEINLINE constexpr bool PowerOfTwo(T value) {
+    return bits::PowerOfTwo<T>(value);
+  }
+  template <typename ... Arguments>
+  static NX_FORCEINLINE constexpr T Mask(Arguments... indexes_) {
+    return bits::Mask<T>(indexes_...);
+  }
+
+
   template <T value_>
   static NX_FORCEINLINE constexpr unsigned int BitScanForward() {
-    return detail::BitScanForward<T, value_>();
+    return bits::BitScanForward<T, value_>();
   }
   template <T value_>
   static NX_FORCEINLINE constexpr unsigned int BitScanReverse() {
-    return detail::BitScanReverse<T, value_>();
+    return bits::BitScanReverse<T, value_>();
   }
   template <T value_>
   static NX_FORCEINLINE constexpr unsigned int PopCount() {
-    return detail::PopCount<T, value_>();
+    return bits::PopCount<T, value_>();
+  }
+  template <T kLHS_, T kRHS_>
+  static NX_FORCEINLINE constexpr bool MultiplicationOverflow() {
+    return bits::MultiplicationOverflow<T, kLHS_, kRHS_>();
+  }
+  template <T value_>
+  static NX_FORCEINLINE constexpr bool PowerOfTwo() {
+    return bits::PowerOfTwo<T, value_>();
+  }
+  template <unsigned int ... indexes_>
+  static NX_FORCEINLINE constexpr T Mask() {
+    return bits::Mask<T,indexes_...>();
   }
 
+
+
+  static NX_FORCEINLINE constexpr unsigned int Size() {
+    return bits::Size<T>();
+  }
 };
 
 
+template <typename Type, Type mask_>
+class BitMasked {
+ public:
+  typedef Type type;
+  static constexpr const Type mask = mask_;
+
+  template <Type value_>
+  static NX_FORCEINLINE constexpr EnableIf<Bool<value_ != 0>,Type> toField() {
+    return std::integral_constant<Type,
+        ((value_ &
+            mpl::LowBitMask<
+                Type, mpl::LowestBitRun<Type, mask_>::length>::value)
+            << mpl::LowestBitRun<Type, mask_>::offset) |
+        Bits<Type, (mask_ & ~(
+            mpl::LowBitMask<
+                Type, mpl::LowestBitRun<Type, mask_>::length>::value
+            << mpl::LowestBitRun<Type, mask_>::offset))>::template toField<
+            (value_ >> mpl::LowestBitRun<Type, mask_>::length)>()
+        >::value;
+  }
+
+
+  template <Type value_>
+  static NX_FORCEINLINE constexpr EnableIf<Bool<value_ == 0>,Type> toField() {
+    return static_cast<Type>(0);
+  }
+  ////
+  // ASSIGN
+  template <Type value_, class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ == static_cast<Type>(0) && // empty
+          Depend<PointerType>::value>,
+      void> assign(PointerType* data) {
+    // empty mask - do nothing
+  }
+  template <Type value_, class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ == ~static_cast<Type>(0) && // full
+          Depend<PointerType>::value>,
+      void> assign(PointerType* data) {
+    // full mask - assign
+    *data = value_;
+  }
+  template <Type value_, class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ != static_cast<Type>(0) && // not empty
+          mask_ != ~static_cast<Type>(0) && // not full
+          (mask_ & value_) == mask_ && // all bits set
+          Depend<PointerType>::value>,
+      void> assign(PointerType* data) {
+    // bit mask, all bits set - or
+    *data |= mask_;
+  }
+  template <Type value_, class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ != static_cast<Type>(0) && // not empty
+          mask_ != ~static_cast<Type>(0) && // not full
+          (mask_ & value_) == static_cast<Type>(0) && // all bits unset
+          Depend<PointerType>::value>,
+      void> assign(PointerType* data) {
+    // bit mask, all bits unset - and inverted mask
+    *data &= ~mask_;
+  }
+  template <Type value_, class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ != static_cast<Type>(0) && // not empty
+          mask_ != ~static_cast<Type>(0) && // not full
+          (mask_ & value_) != static_cast<Type>(0) && // all bits not unset
+          (mask_ & value_) != mask_ && // all bits not set
+          Depend<PointerType>::value>,
+      void> assign(PointerType* data) {
+    // being extra sure that the 'and' is optimized out
+    typedef std::integral_constant<Type,(value_ & mask_)> masked_value;
+    // bit mask, bits not all the same - merge bits
+    *data = masked_value::value | (*data & ~mask_);
+  }
+
+
+  ////
+  // GET
+  template <class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ == static_cast<Type>(0) && // empty
+          Depend<PointerType>::value>,
+      Type> get(PointerType* data) {
+    // empty mask - return nothing
+    return static_cast<Type>(0);
+  }
+  template <class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ == ~static_cast<Type>(0) && // full
+          Depend<PointerType>::value>,
+      Type> get(PointerType* data) {
+    // full mask - return value
+    return *data;
+  }
+  template <class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ != static_cast<Type>(0) && // not empty
+          mask_ != ~static_cast<Type>(0) && // not full
+          Depend<PointerType>::value>,
+      Type> get(PointerType* data) {
+    // bit mask - return bits
+    return (*data & mask_);
+  }
+
+  ////
+  // SET/CLEAR OPERATIONS
+  template <class PointerType>
+  static NX_FORCEINLINE void set(PointerType* data) {
+    assign<~static_cast<Type>(0)>(data);
+  }
+  template <class PointerType>
+  static NX_FORCEINLINE void clear(PointerType* data) {
+    assign<static_cast<Type>(0)>(data);
+  }
+
+  ////
+  // DYNAMIC
+  template <class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ == static_cast<Type>(0) && // empty
+          Depend<PointerType>::value>,
+      void> assign(Type value, PointerType* data) {
+    // empty mask - do nothing
+  }
+  template <class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ == ~static_cast<Type>(0) && // full
+          Depend<PointerType>::value>,
+      void> assign(Type value, PointerType* data) {
+    // full mask - assign
+    *data = value;
+  }
+  template <class PointerType>
+  static NX_FORCEINLINE EnableIf<Bool<
+          mask_ != static_cast<Type>(0) && // not empty
+          mask_ != ~static_cast<Type>(0) && // not full
+          Depend<PointerType>::value>,
+      void> assign(Type value, PointerType* data) {
+    // bit mask - merge bits
+    *data = (value & mask_) | (*data & ~mask_);
+  }
+
+ private:
+  NX_UNINSTANTIABLE(BitMasked);
+};
 }  // namespace nx
 
 #endif  // INCLUDE_NX_BITS_H_
